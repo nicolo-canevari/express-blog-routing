d821fe463d13fb2613835bb83f35f2568c2282ab
nicolo_canevari <nico.canevari@outlook.it>
2025-02-05 15:34:15 +0100

    [FEAT] rotta per lista dei post + show

--- routers/posts.js
+++ routers/posts.js
@@ -1,10 +1,24 @@
 // importo express
 const express = require('express');
-
 // creo un router
 const router = express.Router();
 
 // importo array di posts
 const posts = require('../data/posts');
 
-module.exports = posts;
+// rotta per ottenere la lista dei post
+router.get('/', (req, res) => {
+
+  res.send('Lista dei post');
+
+});
+
+// show per visualizzare un elemento tramite id
+router.get('/:id', function(req, res) {
+
+  res.send('Dettagli dei post ' + reqparams.id);
+
+})
+
+
+// module.exports = posts;
